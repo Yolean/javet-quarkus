07901d14c100ad9309033e75c3a155bc68e830c7
Staffan Olsson <staffan@repos.se>
2021-10-21 09:03:02 +0200

    Runs Javet on localhost:8080/hello

--- src/main/java/se/yolean/quarkustest/javet/GreetingResource.java
+++ src/main/java/se/yolean/quarkustest/javet/GreetingResource.java
@@ -5,12 +5,20 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.caoccao.javet.exceptions.JavetException;
+import com.caoccao.javet.interop.V8Host;
+import com.caoccao.javet.interop.V8Runtime;
+
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+      try (V8Runtime v8Runtime = V8Host.getV8Instance().createV8Runtime()) {
+        return v8Runtime.getExecutor("'Hello Javet\\n'").executeString();
+      } catch (JavetException e) {
+        throw new RuntimeException("TODO handle error", e);
+      }
     }
 }
